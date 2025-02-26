14e0922640486402e2486f379cd995d527e27a97
Nivedita6281 <157876745+Nivedita6281@users.noreply.github.com>
2025-02-26 11:44:51 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
-if not S3_BUCKET_NAME:
-    raise ValueError("S3_BUCKET_NAME is not set in the environment")
